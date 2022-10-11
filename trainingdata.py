aafa925cd97f4f99c71ae9b8ecb0ea9d5f3accb9
morispolanco <morispolanco@gmail.com>
2022-10-11 15:46:24 -0600

    Update trainingdata.py

--- trainingdata.py
+++ trainingdata.py
@@ -1,1 +1,126 @@
+﻿
+training_dataset = [
+    "What rights do persons of disability have?",
+    "Free education for children with benchmark disabilities. Reservation in higher educational institutions. Identification of posts for reservation.Reservation.Incentives to employers in private sector.Special employment exchange.Special schemes and development programmes.",
 
+    "What are the rights of disable people?",
+    "Free education for children with benchmark disabilities. Reservation in higher educational institutions. Identification of posts for reservation.Reservation.Incentives to employers in private sector.Special employment exchange.Special schemes and development programmes.",
+
+    "What are the rights for a person of disability?",
+    "Free education for children with benchmark disabilities. Reservation in higher educational institutions. Identification of posts for reservation.Reservation.Incentives to employers in private sector.Special employment exchange.Special schemes and development programmes.",
+
+
+    "What rights do women and children have?",
+    "According to the act -The appropriate Government and local authorities shall ensure that all children with disabilities shall have right on an equal basis to freely express their views on all matters affecting them and provide them appropriate support keeping in view their age and disability. To view the act click -https://legislative.gov.in/sites/default/files/A2016-49_1.pdf ",
+
+    "What are the rights of women and children?",
+    "According to the act -The appropriate Government and local authorities shall ensure that all children with disabilities shall have right on an equal basis to freely express their views on all matters affecting them and provide them appropriate support keeping in view their age and disability. To view the act click -https://legislative.gov.in/sites/default/files/A2016-49_1.pdf ",
+
+
+    "I am facing discrimination in the workspace.",
+    "According to the law - No Government establishment shall discriminate against any person with disability in any matter relating to employment: Provided that the appropriate Government may, having regard to the type of work carried on in any establishment, by notification and subject to such conditions, if any, exempt any establishment from the provisions of this section. To view the act click -https://legislative.gov.in/sites/default/files/A2016-49_1.pdf ",
+
+    "I have been subjected to discrimination in my workspace.",
+    "According to the law - No Government establishment shall discriminate against any person with disability in any matter relating to employment: Provided that the appropriate Government may, having regard to the type of work carried on in any establishment, by notification and subject to such conditions, if any, exempt any establishment from the provisions of this section. To view the act click -https://legislative.gov.in/sites/default/files/A2016-49_1.pdf ",
+
+    "I need a lawyer",
+    "Here you go, the best platform for all your legal needs - https://vakilsearch.com/ ",
+
+    "I want a lawyer",
+    "Here you go, the best platform for all your legal needs - https://vakilsearch.com/ ",
+
+    "I need a legal advisor",
+    "Here you go, the best platform for all your legal needs - https://vakilsearch.com/ ",
+
+    "How can i avail an insurance scheme?",
+    "The appropriate Government shall, by notification, make insurance schemes for their employees with disabilities. For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "I am looking for a rehab.",
+    "The appropriate Government and the local authorities shall within their economic capacity and development, undertake or cause to be undertaken services and programmes of rehabilitation, particularly in the areas of health, education and employment for all persons with disabilities. For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "Can I play sports?",
+    "The sports authorities shall accord due recognition to the right of persons with disabilities to participate in sports and shall make due provisions for the inclusion of persons with disabilities in their schemes and programmes for the promotion and development of sporting talents. For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "I am not allowerd to play sports because I am disabled",
+    "The sports authorities shall accord due recognition to the right of persons with disabilities to participate in sports and shall make due provisions for the inclusion of persons with disabilities in their schemes and programmes for the promotion and development of sporting talents. For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "I am not getting admission in a school because of my disability, is this legal? ",
+    "Notwithstanding anything contained in the Rights of Children to Free and Compulsory Education Act, 2009 (35 of 2009), every child with benchmark disability between the age of six to eighteen years shall have the right to free education in a neighbourhood school, or in a special school, of his choice. (2) The appropriate Government and local authorities shall ensure that every child with benchmark disability has access to free education in an appropriate environment till he attains the age of eighteen years.For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "Am I allowed to get admission if I am disabled?",
+    "Notwithstanding anything contained in the Rights of Children to Free and Compulsory Education Act, 2009 (35 of 2009), every child with benchmark disability between the age of six to eighteen years shall have the right to free education in a neighbourhood school, or in a special school, of his choice. (2) The appropriate Government and local authorities shall ensure that every child with benchmark disability has access to free education in an appropriate environment till he attains the age of eighteen years.For further assistance, please visit - https://vakilsearch.com/",
+
+    "How much reservation do I have as a disabled person?",
+    "All Government institutions of higher education and other higher education institutions receiving aid from the Government shall reserve not less than five per cent. seats for persons with benchmark disabilities. (2) The persons with benchmark disabilities shall be given an upper age relaxation of five years for admission in institutions of higher education.For further assistance, please visit - https://vakilsearch.com/",
+
+    "Can I access reservation ?",
+    "All Government institutions of higher education and other higher education institutions receiving aid from the Government shall reserve not less than five per cent. seats for persons with benchmark disabilities. (2) The persons with benchmark disabilities shall be given an upper age relaxation of five years for admission in institutions of higher education.For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "Do I have reservation?",
+    "All Government institutions of higher education and other higher education institutions receiving aid from the Government shall reserve not less than five per cent. seats for persons with benchmark disabilities. (2) The persons with benchmark disabilities shall be given an upper age relaxation of five years for admission in institutions of higher education.For further assistance, please visit - https://vakilsearch.com/",
+
+    "Can I get incentives in my employment ?",
+    "The appropriate Government and the local authorities shall, within the limit of their economic capacity and development, provide incentives to employer in private sector to ensure that at least five per cent. of their work force is composed of persons with benchmark disability.For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "Do I have special rights in my employment? ",
+    "The appropriate Government and the local authorities shall, within the limit of their economic capacity and development, provide incentives to employer in private sector to ensure that at least five per cent. of their work force is composed of persons with benchmark disability.For further assistance, please visit - https://vakilsearch.com/",
+
+    "Does the government run awareness campaigns ?",
+    "The appropriate Government, in consultation with the Chief Commissioner or the State Commissioner, as the case may be, shall conduct, encourage, support or promote awareness campaigns and sensitisation programmes to ensure that the rights of the persons with disabilities provided under this Act are protected.For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "How do I access public transport?",
+    "The act states that - The appropriate Government shall take suitable measures to provide,— (a) facilities for persons with disabilities at bus stops, railway stations and airports conforming to the accessibility standards relating to parking spaces, toilets, ticketing counters and ticketing machines;(b) access to all modes of transport that conform the design standards, including retrofitting oldmodes of transport, wherever technically feasible and safe for persons with disabilities, economically viable and without entailing major structural changes in design;  (c) accessible roads to address mobility necessary for persons with disabilities.(2) The appropriate Government shall develop schemes programmes to promote the personal mobility of persons with disabilities at affordable cost to provide for,— (a) incentives and concessions; (b) retrofitting of vehicles; and (c) personal mobility assistance.For further assistance, please visit - https://vakilsearch.com/  ",
+
+    "How do I make an appeal ?",
+    "Any person aggrieved by the order of the competent authority refusing to grant a certificate of registration or revoking a certificate of registration may, within such period as may be prescribed by the State Government, prefer an appeal to such appellate authority, as may be notified by the State Government against such refusal or revocation.For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "Can I make an appeal?",
+    "Any person aggrieved by the order of the competent authority refusing to grant a certificate of registration or revoking a certificate of registration may, within such period as may be prescribed by the State Government, prefer an appeal to such appellate authority, as may be notified by the State Government against such refusal or revocation.For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "I want a special certification for my disabilities",
+    "The appropriate Government shall designate persons, having requisite qualifications and experience, as certifying authorities, who shall be competent to issue the certificate of disability. For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "I have a special disability, I need a certificate to prove that",
+    "The appropriate Government shall designate persons, having requisite qualifications and experience, as certifying authorities, who shall be competent to issue the certificate of disability.For further assistance, please visit - https://vakilsearch.com/",
+
+    "What is the procedure for certifying that I am a disabled person?",
+    "Any person with specified disability, may apply, in such manner as may be prescribed by the Central Government, to a certifying authority having jurisdiction, for issuing of a certificate of disability. For further assistance, please visit - https://vakilsearch.com/",
+
+    "Do disable persons have a special court?",
+    "For the purpose of providing speedy trial, the State Government shall, with the concurrence of the Chief Justice of the High Court, by notification, specify for each district, a Court of Session to be a Special Court to try the offences under this Act. For further assistance, please visit - https://vakilsearch.com/",
+
+    "Do disable people have special funds?",
+    "There shall be constituted a Fund to be called the State Fund for persons with disabilities by a State Government in such manner as may be prescribed by the State Government.For further assistance, please visit - https://vakilsearch.com/ ",
+
+    "Do disable people have access to a special public prosecutor?",
+    "For every Special Court, the State Government may, by notification, specify a Public Prosecutor or appoint an advocate, who has been in practice as an advocate for not less than seven years, as a Special Public Prosecutor for the purpose of conducting cases in that CourtFor further assistance, please visit - https://vakilsearch.com/ ",
+
+    "What are the categories of disabilities?",
+    "You can be categorized into - Physical disability, Visual impairment ,Hearing impairment,speech and language disability,  Intellectual disability.  Any other category as may be notified by the Central Government. For further assistance, please visit - https://vakilsearch.com/ ",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    "when was the THE RIGHTS OF PERSONS WITH DISABILITIES ACT formed ?",
+    "2016",
+
+
+
+
+]
+{
+    'import_path': 'chatterbot.logic.SpecificResponseAdapter',
+    'input_text': 'Help',
+    'output_text': 'Ok, here is a link: https://vakilsearch.com/'
+}
